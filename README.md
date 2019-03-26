3bb4f8d48c7665c3245987b4f98cfbc0847a138b
idan22moral <idan22moral@gmail.com>
2019-03-26 23:12:56 +0200

    Improved comments and changed the way of handling regions

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # RedAlert
 Simple python program that notifies Red Alerts
+
+## Simple Explanation
+This script shows a Windows notification to the user that contains the numbers of regions that just recieved Red Alert.
+
+The regions that will show the user a notification are writen in a configuration file called `regions.cfg`, and it should be located under the same directory as the Python script.
+
+In the console you will see all the alerted regions, even if you configuted it to not notify you (you can think of it as a 'silent' notification).
+
+## The `regions.cfg` File
+In order to get alerts of specific regions only, you need to configure the program.
+To do that, you simply need to add a file called `regions.cfg` in the same directory as the Python script.
+The proper format of the configuration file is a list numbers that represent the regions, like that:
+```
+219
+246
+247
+218
+```
+By using these numbers, you will recieve a notification when a Red Alert is heard in one of those regions.
+To know the region numbers by a city, use the Pikud-Ha'Oref feature [here](https://www.oref.org.il/11093-he/Pakar.aspx).
+
+## Installation
+1. Run `pip install -r requirements.txt` to install all the required libraries.
+
+## How To Run It
+You can run the script in the same way you run any Python script.
+Just make sure that you use a Python 3.6 or any other Python 3 version.
+
+## Notes
+This is a simple script that should represent the basic idea.
+Feel free to improve the implementation, add features, add GUI, etc.
+Just fork this repository, I would love to see your work!
--- pikud.py
+++ pikud.py
@@ -9,36 +9,43 @@ import os
 REGIONS_FILE_PATH = "regions.cfg"
 PIKUD_URL = "https://www.oref.org.il/WarningMessages/alert/alerts.json"
 PIKUD_REFERER = "https://www.oref.org.il/894-he/pakar.aspx"
+USER_REGIONS = set()
 CURRENT_ALERTS = set()
 ALERT_TIME = 30
 NOTIFIER = ToastNotifier()
 
 
 
-def load_regions():
+def load_regions() -> set():
     """
     Loads the regions that interest the user from the regions.cfg file.
     """
-    # Make sure that the config file exists before trying to access it
-    if os.path.exists(REGIONS_FILE_PATH):
+    global USER_REGIONS
+
+    try:
         # Load the regions from the config file
-        USER_REGIONS = open(REGIONS_FILE_PATH ,"r").readlines()
-        
+        with open(REGIONS_FILE_PATH ,"r") as f:
+            USER_REGIONS = f.readlines()
+    
         # Remove '\n' from the end of every line, and remove empty lines
         USER_REGIONS = list(map(lambda line: line.strip(), USER_REGIONS))
         USER_REGIONS = set(filter(lambda line: line != '', USER_REGIONS))
-        
+
         print(f"Found {len(USER_REGIONS)} regions in the config file.")
         if len(USER_REGIONS) > 0:
             print("You'll receive a notification when Red Alert hits these regions:")
             print(', '.join(USER_REGIONS))
-    else:
+    except FileNotFoundError:
+        # Set USER_REGIONS as None to indicate the fact that the user has no prefrences
+        # In this case the user shoud recieve an alert for every possible region
+        USER_REGIONS = None
         print('No /regions.cfg file found.')
+    
     print('-' * 32)
     return USER_REGIONS
 
 
-def get_current_alerts():
+def get_current_alerts() -> str:
     """
     Returns a string that contains the json data from the alert source.
     Note: when there are no alerts, the string is empty.
@@ -51,26 +58,50 @@ def get_current_alerts():
     return response.text
 
 
-def notify_user(region):
+def notify_user(region: str) -> None:
     """
-    Shows an Windows alert to the user that contains the region name.
+    Shows a Windows alert to the user that contains the region name.
     """
-    NOTIFIER.show_toast("Red Alert!", msg=region)
-    
+    NOTIFIER.show_toast("Red Alert!", "A")
 
-def handle_region(region):
+
+def handle_region(region: str) -> None:
     """
-    Saves the region in the alerts list, and removes it when the time comes. 
+    Sets a timer of ALERT_TIME seconds, and notifies the user about the region.
+    When ALERT_TIME seconds pass, new alerts from the given region can be alerted.
+    We use a timer to prevent alert flooding about the same region.
     """
+    # Check if region is in the user's regions set
+    # and make sure that the region is not already alerted
+    
     if region in USER_REGIONS and region not in CURRENT_ALERTS:
+        # Add the region to the list of current alerts
+        CURRENT_ALERTS.add(region)
+        # Set a timer for ALERT_TIME
+        # When it ends the region will be removed from the alerts list
+        timer = threading.Timer(ALERT_TIME, lambda: CURRENT_ALERTS.remove(region))
+        timer.start()
+        # Notify the user about the red alert in the given region
+        notify_user(region)
+
+
+def alert_regions(regions: list) -> None:
+    # Remove already alerted regions from the list
+    regions_to_alert = [region for region in regions if region not in CURRENT_ALERTS] 
+    
+    # Remove the irrelevant regions from the list, if the user has any prefrences
+    if USER_REGIONS != None:
+        regions_to_alert = [region for region in regions if region in USER_REGIONS]
+    
+    for region in regions_to_alert:
         CURRENT_ALERTS.add(region)
         timer = threading.Timer(ALERT_TIME, lambda: CURRENT_ALERTS.remove(region))
         timer.start()
-        notify_user(region)
-        print("Notified")
+    print("Notified")
+    notify_user(', '.join(regions_to_alert))
+    
 
-
-def print_silent_alerts(regions):
+def print_silent_alerts(regions: list) -> None:
     """
     Prints all the regions to the console with a timestamp.
     """
@@ -96,13 +127,18 @@ def main():
         if current_alerts_raw is not '':
             # Convert the raw json data from string to dictionary
             current_alerts_data = json.loads(current_alerts_raw)
-            # Handle every region in the current alerts
-            for region in current_alerts_data["data"]:
-                handle_region(region)
-            # Print all the current regions to the console as "silent" alerts
-            print_silent_alerts(current_alerts_data["data"])
             
-        # Make the request to Pikud Ha'Oref's link every 1 second
+            try:
+                # Handle every region in the current alerts
+                #for region in current_alerts_data["data"]:
+                #    handle_region(region)
+                alert_regions(current_alerts_data["data"])
+                # Print all the current regions to the console as "silent" alerts
+                print_silent_alerts(current_alerts_data["data"])
+            except Exception as e:
+                print(f'Error: {str(e)}')
+            
+        # Make the request to Pikud-Ha'Oref's link every 1 second
         time.sleep(1)
 
 if __name__ == "__main__":
